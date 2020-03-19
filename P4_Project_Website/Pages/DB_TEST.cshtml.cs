5a45c3ff0c972c66f91dcba7f12bf0c2c6d2e1b5
orn <ornapache@live.com>
2020-03-19 12:59:38 +0100

    DB-TEST side

---
+++ P4_Project_Website/Pages/DB_TEST.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace P4_Project_Website.Pages
+{
+    public class DB_TESTModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
