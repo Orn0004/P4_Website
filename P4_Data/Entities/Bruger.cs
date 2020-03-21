73f9bafa2358340055711c1cd111e0124488f057
Dennis Tran <dtran18@student.aau.dk>
2020-03-21 13:18:22 +0100

    added comments for future reference
    comments are written in English.

--- P4_Data/Entities/Bruger.cs
+++ P4_Data/Entities/Bruger.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace P4_Data.Entities
 {
+    // a class that contains the different entities and allows us to modify and see it.
     public class Bruger
     {
         public string Fornavn { get; set; }
