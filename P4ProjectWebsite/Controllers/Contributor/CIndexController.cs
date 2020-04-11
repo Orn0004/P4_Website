ba1ce0c5d94f8a5aca16b667962eeaf3635ff473
fonzen1 <48014544+fonzen1@users.noreply.github.com>
2020-04-11 23:57:04 +0200

    View change depending on role (not finished)

---
+++ P4ProjectWebsite/Controllers/Contributor/CIndexController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P4ProjectWebsite.Controllers.Contributor
+{
+    //[Authorize(Roles = "Admin")]
+    public class CIndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    return RedirectToAction("Index", "Contributor");
+            //}
+
+            return View();
+        }
+    }
+}
--- P4ProjectWebsite/Controllers/HomeController.cs
+++ P4ProjectWebsite/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using P4ProjectWebsite.Models;
 
 namespace P4ProjectWebsite.Controllers
-{
+{ 
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
